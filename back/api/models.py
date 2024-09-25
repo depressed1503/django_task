4fe48da218885455e829737c9e45c8c902ed2699
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-09-25 16:38:31 +0300

    Added min value validator for price.

--- back/api/models.py
+++ back/api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Good(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название товара')
-    description = models.TextField(verbose_name='Описание товара')
-    price = models.IntegerField(default=0)
+    name = models.CharField(max_length=255, verbose_name='Название товара', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание товара', blank=True, null=True)
+    price = models.IntegerField(default=0, blank=False, null=False, validators=[MinValueValidator(0)])
